4ca60cb022a328709991d694ff61e321201bdb63
Brian Lima <brian.ghwt@gmail.com>
2014-09-06 17:56:49 -0300

    Initial commit
    Almost done, simple app.

---
+++ PhoneApp2/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using CortanaRepeat.Resources;
+
+namespace CortanaRepeat
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
