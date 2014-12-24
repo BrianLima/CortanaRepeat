9d0eeeb7bcc67d36d5f1cd46251148238f2d886e
Brian Lima <Brian.ghwt@gmail.com>
2014-12-23 23:49:14 -0200

    Added CortanaOverlayData

---
+++ RepeatforCortana/CortanaOverlayData.cs
@@ -0,0 +1,12 @@
+﻿namespace RepeatforCortana
+{
+    class CortanaOverlayData
+    {
+        public CortanaOverlayData(string Title, string Message)
+        {
+            this._title = Title;
+        }
+        private string _title { get; set; }
+        private string _message { get; set; }
+    }
+}
